4ce09820216fdbb5d9e63d0ff5e5fba5131ff2f3
luetkemj <luetkemj@gmail.com>
2025-06-17 07:10:17 -0600

    reduce diagnostic update freq

--- init.lua
+++ init.lua
@@ -636,6 +636,8 @@ require('lazy').setup({
       -- Diagnostic Config
       -- See :help vim.diagnostic.Opts
       vim.diagnostic.config {
+        -- Reduce diagnostic update frequency
+        update_in_insert = false, -- Don't update diagnostics in insert mode
         severity_sort = true,
         float = { border = 'rounded', source = 'if_many' },
         underline = { severity = vim.diagnostic.severity.ERROR },
@@ -650,15 +652,12 @@ require('lazy').setup({
         virtual_text = {
           source = 'if_many',
           spacing = 2,
+          -- Simplify the format function
           format = function(diagnostic)
-            local diagnostic_message = {
-              [vim.diagnostic.severity.ERROR] = diagnostic.message,
-              [vim.diagnostic.severity.WARN] = diagnostic.message,
-              [vim.diagnostic.severity.INFO] = diagnostic.message,
-              [vim.diagnostic.severity.HINT] = diagnostic.message,
-            }
-            return diagnostic_message[diagnostic.severity]
+            return diagnostic.message
           end,
+          -- Only show errors and warnings in virtual text
+          severity = { min = vim.diagnostic.severity.WARN },
         },
       }
 
