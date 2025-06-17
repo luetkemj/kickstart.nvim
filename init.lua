955c844ebab197825df8350f870604439ae57c5b
luetkemj <luetkemj@gmail.com>
2025-06-17 07:04:19 -0600

    Update init.lua

--- init.lua
+++ init.lua
@@ -588,6 +588,7 @@ require('lazy').setup({
           --    See `:help CursorHold` for information about when this is executed
           --
           -- When you move your cursor, the highlights will be cleared (the second autocommand).
+          local client = vim.lsp.get_client_by_id(event.data.client_id)
           if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
             local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
 
